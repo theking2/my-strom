61860db2c8c5cbaa64219b2f33d94a16709abd43
theking2 <johannes.kingma@gamil.com>
2021-09-21 20:56:39 +0200

    error handling in toggle.php

--- ajax/toggle.php
+++ ajax/toggle.php
@@ -3,5 +3,7 @@ $ch = curl_init();
 $url = "http://" . $_GET["switch-ip"] . "/toggle";
 curl_setopt( $ch, CURLOPT_URL, $url );
 curl_setopt( $ch, CURLOPT_HEADER, 0 );
-curl_exec( $ch );
+if( true!==curl_exec( $ch ) ) {
+	error_log("toggle failed: $url");
+}
 curl_close( $ch );
