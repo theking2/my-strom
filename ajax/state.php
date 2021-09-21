f286be49d0031468cb0c74daf6f8e172ca28fd74
theking2 <johannes.kingma@gamil.com>
2021-09-21 20:57:09 +0200

    timeout in state.php

--- ajax/state.php
+++ ajax/state.php
@@ -4,5 +4,6 @@ error_log($_GET["switch-ip"]);
 $url = "http://" . $_GET["switch-ip"] . "/report";
 curl_setopt( $ch, CURLOPT_URL, $url );
 curl_setopt( $ch, CURLOPT_HEADER, 0 );
+curl_setopt( $ch, CURLOPT_TIMEOUT, 500 );
 curl_exec( $ch );
 curl_close( $ch );
