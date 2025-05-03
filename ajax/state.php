a6b63fe155f86e2df762b161cdad7c244dc13705
theking2 <theking2@king.ma>
2025-05-03 20:25:41 +0200

    no need for these

--- ajax/state.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ch = curl_init();
-error_log($_GET["switch-ip"]);
-$url = "http://" . $_GET["switch-ip"] . "/report";
-curl_setopt( $ch, CURLOPT_URL, $url );
-curl_setopt( $ch, CURLOPT_HEADER, 0 );
-curl_setopt( $ch, CURLOPT_TIMEOUT, 500 );
-curl_exec( $ch );
-curl_close( $ch );
